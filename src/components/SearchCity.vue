55625a9ed0dc8af7ba4c5b9fbe8c297f930ad596
cecidragonetti94 <cecidragonetti@hotmail.com>
2021-10-26 09:22:09 -0300

    search

---
+++ src/components/SearchCity.vue
@@ -0,0 +1,25 @@
+.<template>
+  <v-form @submit.prevent="handleCity" class="form-search">
+      <v-text-field v-model="city" label="Enter City"></v-text-field>
+      <v-btn color="success" type="submit">Search</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            city: '',
+        }
+    },
+    methods:{
+        handleCity(){
+            this.$emit("accion", this.city)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/WeatherCity.vue
+++ src/views/WeatherCity.vue
@@ -1,13 +1,15 @@
 <template>
 <div>
+  <SearchCity @accion="getCity"/>
   <City :data="city"/>
   </div>
 </template>
 
 <script>
 import City from '../components/City.vue'
+import SearchCity from '../components/SearchCity.vue'
 export default {
-  components: { City },
+  components: { City, SearchCity },
   data(){
     return{
    city:{}
@@ -18,12 +20,13 @@ export default {
     this.getCity()
   },
   methods:{
-    async getCity(){
+    async getCity(city){
       const key = "4af561725919418cf813bd0a77f4b185"
-      const {data} = await this.axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`)
+      const {data} = await this.axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
       this.city = data
+      console.log("city", city)
       console.log(this.city)
-      console.log(data.main.humidity)
+      
     }
   }
 }
