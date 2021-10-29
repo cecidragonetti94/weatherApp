989c9cbf390efb2e3d3d2a19316a49937dfeee99
cecidragonetti94 <cecidragonetti@hotmail.com>
2021-10-29 10:41:37 -0300

    refactor

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="text-center">
+  <div class="text-center" col=12>
     <h1>Ciudades de Argentina</h1>
     <v-card class="mx-auto" max-width="400" color="secondary" v-for="item in this.data.list" :key="item.id">
      <v-card-title>
-       {{item.name}}
+       {{item.name}} - {{item.sys.country}}
        </v-card-title> 
+       <div class="datos">
+      
+        <h4><span>Weather:</span>  {{ item.weather[0].description }}</h4>
+        <h4><span>Humidity:</span>  {{ item.main.humidity }}%</h4>
+        <h4><span>Pressure:</span>  {{ item.main.pressure }}</h4>
+          <h2><span>Tempeture:</span>  {{ item.main.temp}}°</h2> 
+         <h3><span>Max tempeture:</span>  {{ item.main.temp_max}}°</h3>
+        <h3><span>Min tempeture:</span>  {{ item.main.temp_min}}°</h3>  
+       
+      </div>
     </v-card>
 
     <h1>Favorites</h1>
@@ -16,8 +26,19 @@
 <script>
 
 export default {
-  props: {data: Object}
-};
+  props: {data: Object},
+  data(){
+    return{
+      kelvin:273.15,
+    }
+  },
+//  methods: {
+//  tempeture() {
+//    const temp = this.data.list.main.temp - this.kelvin;
+//    return Math.round(temp);
+//  },
+// }
+}
 </script>
 
 <style>
--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -17,8 +17,8 @@ export default {
   methods:{
    async getBbox(){
      const key = "4af561725919418cf813bd0a77f4b185"
-     const lat = -34.6132
-     const lon = -58.3772
+     const lat = -38.416097
+     const lon = -63.616672
      const cant = 10
      const {data} = await this.axios.get(`https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=${cant}&appid=${key}`)
     this.bbox = data
